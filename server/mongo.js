41cc43d10584feb83a018228c59aea6336b12fc5
xueq <762452134@qq.com>
2020-11-12 09:22:28 +0800

    调整辣椒tv客户端

--- server/mongo.js
+++
@@ -1,7 +0,0 @@
-
- 
-// MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-//   if (err) throw err;
-//   db.db('chili').collection("account").find({}).toArray(function(err, result){console.log(result)})
-//   db.close();
-// });
